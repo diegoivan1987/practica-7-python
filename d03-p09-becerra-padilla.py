c67e989b66bb36ca6634f285a162573bfebe70a8
diego <diegoivan1987@outlook.com>
2019-10-10 21:27:51 -0500

    2 version del menu

--- d03-p09-becerra-padilla.py
+++ d03-p09-becerra-padilla.py
@@ -2,47 +2,65 @@
 #becerra gonzalez diego ivan
 #padilla valdez gustavo
 
-def e1():
-        print("\ejercicio 1");
-def e2():
-        print("\ejercicio 2");
-def e3():
-        print("\ejercicio 3");
-def e4():
-        print("\ejercicio 4");
-def e5():
-        print("\ejercicio 5");
-def e6():
-        print("\ejercicio 6");
-def e7():
-        print("\ejercicio 8");
+def e1():#funcion para el ejercicio 1
+    print("\nEjercicio 1");
 
-def menu():#muestra el menu
-    i = int(input("Ingresa el numero de ejercicio a ejecutar y 8 para salir"));
-    for i in range (8):
-        
-        if i == 1 :
-            e1();
-            i = 0;
-        elif i == 2 :
-            e2();
-            i = 0;
-        elif i == 3 :
-            e3();
-            i = 0;
-        elif i == 4 :
-            e4();
-            i = 0;
-        elif i == 5 :
-            e5();
-            i = 0;
-        elif i == 6 :
-            e6();
-            i = 0;
-        elif i == 7 :
-            e7();
-            i = 0;
-        i = int(input("Ingresa el numero de ejercicio a ejecutar y 8 para salir"));
-        i=0;
+def e2():#funcion para el ejercicio 2
+    print("\nEjercicio 2");
+
+def e3():#funcion para el ejercicio 3
+    print("\nEjercicio 3");
+
+def e4():#funcion para el ejercicio 4
+    print("\nEjercicio 4");
+
+def e5():#funcion para el ejercicio 5
+    print("\nEjercicio 5");
+   
+def e6():#funcion para el ejercicio 6
+    print("\nEjercicio 6");
+
+def e7():#funcion para el ejercicio 7
+    print("\nEjercicio 7");
+
+def menu():#muestra un menu para ejecutar los ejercicios
+        aumento = 1 ;#sera la variable que hace infinito el ciclo
+        for i in range(aumento) :#se repetira hasta llegar a 1, al llegar a una opcion o validacion i se inicia en 0
+                opcion = str(input("\nIngresa el numero de ejercicio a ejecutar y 8 para salir "));
+                try :#si ingresa un numero, lo ejecuta
+                        opcion = int(opcion);
+                        if opcion < 1 or opcion > 8 :#si esta fuera del rango o es negativo
+                                opcion = str(input("Debes ingresar un numero "));
+                        else :#en otro caso
+                                if opcion == 1 :#ejecuta el ejercicio 1
+                                        e1();
+                                        aumento += 1;
+                                if opcion == 2 :#ejecuta el ejercicio 2
+                                        e2();
+                                        aumento += 1;
+                                if opcion == 3 :#ejecuta el ejercicio 3
+                                        e3();
+                                        aumento += 1;
+                                if opcion == 4 :#ejecuta el ejercicio 4
+                                        e4();
+                                        aumento += 1;
+                                if opcion == 5 :#ejecuta el ejercicio 5
+                                        e5();
+                                        aumento += 1;
+                                if opcion == 6 :#ejecuta el ejercicio 6
+                                        e6();
+                                        aumento += 1;
+                                if opcion == 7 :#ejecuta el ejercicio 7
+                                        e7();
+                                        aumento += 1;
+                                
+                except :#en caso de que ingrese un caracter
+                        opcion = str(input("Debes ingresar un numero "));
+                        aumento += 1;
+                        
+
 menu();
-print("\nPractica 9");
+
+print("\npractica 09");
+print("becerra gonzalez diego ivan");
+print("padilla valdez gustavo");
