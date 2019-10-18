684b6124bf94f1f5c070a00c0812d2cbc0ab403f
Piloalucard <piloalucard@gmail.com>
2019-10-17 20:13:27 -0500

    Archivo practica 10 añadido

---
+++ d03-p10-padilla-becerra.py
@@ -0,0 +1,24 @@
+#practica 10
+#padilla valdez gustavo
+#becerra gonzalez diego ivan
+
+
+
+def e1():
+    pass;
+def e2():
+    pass;
+def e3():
+    pass;
+def e4():
+    pass;
+def menu():
+    pass;
+
+
+menu();
+
+print("\npractica 10");
+print("padilla valdez gustavo");
+print("becerra gonzalez diego ivan");
+
