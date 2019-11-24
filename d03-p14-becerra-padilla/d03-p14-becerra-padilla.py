31c5bb62b7ade4451551a85c0e164f576d04e72d
diego <diegoivan1987@outlook.com>
2019-11-24 11:56:20 -0600

    se corrigio el menu

--- d03-p14-becerra-padilla/d03-p14-becerra-padilla.py
+++ d03-p14-becerra-padilla/d03-p14-becerra-padilla.py
@@ -10,54 +10,66 @@ init(autoreset=True); #Se inicia el colorama, que se autoresetee para evitar bas
 
 def ventana(xM,yM,seccion): #Esta funcion creara la ventana siempre que se necesite con colores
     os.system("cls"); #Borrara la ventana
-    #Los siguiente 4 for sera para hacer el marco de la ventana
-    x=1;
-    y=3;
+    #Los siguiente 4 while sera para hacer el marco de la ventana
+    
     aux=0;
-    for i in range(xM):
+    
+    x=(120-xM)//2;
+    y=(30-yM)-1;
+
+    limiteX = x + xM;
+    limiteY = y +yM;
+
+    while x <= limiteX:
         print(Cursor.POS(x,y)+Back.BLUE+" ");
         x += 1;
-
-    for i in range(yM):
-        print(Cursor.POS(x,y)+Back.BLUE+"  ");
-        y += 1;
-    x=1;
-    y=3;
-    for i in range(yM):
+    
+    while y <= limiteY:
         print(Cursor.POS(x,y)+Back.BLUE+"  ");
         y += 1;
 
-    for i in range(xM+2):
-        print(Cursor.POS(x,y)+Back.BLUE+" ");
-        x+=1;
+    x=(120-xM)//2;
+    y=(30-yM)-1;
+    while y <= limiteY-1:
+        print(Cursor.POS(x,y)+Back.BLUE+"  ");
+        y += 1;
+
+    while x <= limiteX:
+        print(Cursor.POS(x,y)+Back.BLUE+" ",end="");
+        x += 1;
+
     #Los siguientes crearan el mini adornado tambien en el marco
-    x=3;
-    y=4;
-    for i in range(xM-4):
+    x=(120-xM)//2+2;
+    y=(30-yM);
+
+    while x <= limiteX-2:
         print(Cursor.POS(x,y)+Back.LIGHTBLUE_EX+Fore.BLACK+"*");
         x += 1;
-    for i in range(yM-1):
-        print(Cursor.POS(x,y)+Back.LIGHTBLUE_EX+Fore.BLACK+"**");
-        y += 1;    
-    x=3;
-    y=4;
-    for i in range(yM-2):
+    
+    while y <= limiteY-1:
         print(Cursor.POS(x,y)+Back.LIGHTBLUE_EX+Fore.BLACK+"**");
         y += 1;
 
-    for i in range(xM-4):
-        print(Cursor.POS(x,y)+Back.LIGHTBLUE_EX+Fore.BLACK+"*");
-        x+=1;
+    x=(120-xM)//2+2;
+    y=(30-yM);
+    while y <= limiteY-2:
+        print(Cursor.POS(x,y)+Back.LIGHTBLUE_EX+Fore.BLACK+"**");
+        y += 1;
+
+    while x <= limiteX-2:
+        print(Cursor.POS(x,y)+Back.LIGHTBLUE_EX+Fore.BLACK+"*",end="");
+        x += 1;
 
     #Estos for crean la "sombra" de la ventana
-    x=5;
-    y=2;
-    for i in range(xM+1):
+    x=(120-xM)//2+2;
+    y=(30-yM)-2;
+    while x <= limiteX+3:
         print(Cursor.POS(x,y)+Back.LIGHTBLACK_EX+" ");
-        x += 1;
-    x=xM+3;
-    for i in range(yM):
-        print(Cursor.POS(x,y)+Back.LIGHTBLACK_EX+"   ");
+        x += 1;   
+    
+    while y <= limiteY-1:
+        print(Cursor.POS(x-1,y)+Back.LIGHTBLACK_EX+" ");
+        print(Cursor.POS(x,y)+Back.LIGHTBLACK_EX+" ");
         y += 1;
     
     archivo = open("d03-p14-becerra-padilla-H.txt",mode = "r",encoding="utf-8"); #Abrira el archivo del hospital
@@ -67,84 +79,95 @@ def ventana(xM,yM,seccion): #Esta funcion creara la ventana siempre que se neces
     dom=str(dom);
     archivo.close();
 
+    centro_ventana = limiteX //2;
+    y=(30-yM);
+
+    aux=int(len(seccion)); #Encuentra la mitad de la ventana para imprimir la seccion
+    aux=aux//2;
+    print(Fore.LIGHTYELLOW_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(centro_ventana-aux,y)+seccion, end="");
+
     aux=int(len(hosp)); #Encuentra la mitad de la ventana para imprimir los datos del hospital
-    aux=xM-aux;
     aux=aux//2;
     #Los imprime
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(aux+2,3)+hosp, end="");
-    aux=int(len(seccion)); #Encuentra la mitad de la ventana para imprimir los datos del hospital
-    aux=xM-aux;
-    aux=aux//2;
-    print(Fore.LIGHTYELLOW_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(aux+2,6)+seccion, end="");
-    print(Fore.LIGHTBLACK_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(xM-len(dom)-1,5)+dom, end="");
-    print(Fore.LIGHTRED_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(2,28)+"->PoliProgramming®<-", end="");
-    print(Fore.LIGHTRED_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(2,29)+"->Becerra Gonzalez Diego Ivan<-", end="");
-    print(Fore.LIGHTRED_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(2,30)+"->Padilla Valdez Gustavo<-", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(centro_ventana-aux,y+1)+hosp, end="");
+    print(Fore.LIGHTBLACK_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(centro_ventana-aux,y+2)+dom, end="");
+
+    x=(120-xM)//2;
+    limiteY = y +yM;
+    
+    print(Fore.LIGHTRED_EX+Back.BLACK+Style.BRIGHT+Cursor.POS(x+4,limiteY-3)+"->Padilla Valdez Gustavo, Becerra Gonzalez Diego Ivan, PoliProgramming®<-", end="");
 
 #Crea las diferencias de menu, dependiendo de donde se encuentre la opcion 
 def opmenu1(xM,yM):
+    x=(120-xM)//2+4;
+    y=(30-yM)+2;
     pac="1.-Alta de Pacientes"
     med="2.-Alta de Medicos"
     hosp="3.-Alta de datos de Hospital"
     sal="4.-Salir"
     #Los cY y cX son para encontrar el centro de la pantalla dependiendo y colocar cada linea donde es
-    cY=yM+2;
-    cY=cY//2;
-    cX=int(len(hosp));
-    cX=xM-cX;
-    cX=cX//2;
-    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX+2,cY)+pac+" <--", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+1)+med+"    ", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+2)+hosp+"    ", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+3)+sal+"    ", end="");
+    cY=(y+yM)//2;
+    cX=(x+xM)//2;
+    aux = len(hosp)//2;
+    cX = cX - aux;
+    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX,cY)+pac+" <--", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+1)+med+"    ", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+2)+hosp+"    ", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+3)+sal+"    ", end="");
+
 #Crea las diferencias de menu, dependiendo de donde se encuentre la opcion 
 def opmenu2(xM,yM):
+    x=(120-xM)//2+4;
+    y=(30-yM)+2;
     pac="1.-Alta de Pacientes"
     med="2.-Alta de Medicos"
     hosp="3.-Alta de datos de Hospital"
     sal="4.-Salir"
     #Los cY y cX son para encontrar el centro de la pantalla dependiendo y colocar cada linea donde es
-    cY=yM+2;
-    cY=cY//2;
-    cX=int(len(hosp));
-    cX=xM-cX;
-    cX=cX//2;
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY)+pac+"    ", end="");
-    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX+2,cY+1)+med+" <--", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+2)+hosp+"    ", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+3)+sal+"    ", end="");
+    cY=(y+yM)//2;
+    cX=(x+xM)//2;
+    aux = len(hosp)//2;
+    cX = cX - aux;
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY)+pac+"    ", end="");
+    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX,cY+1)+med+" <--", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+2)+hosp+"    ", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+3)+sal+"    ", end="");
+
 #Crea las diferencias de menu, dependiendo de donde se encuentre la opcion 
 def opmenu3(xM,yM):
+    x=(120-xM)//2+4;
+    y=(30-yM)+2;
     pac="1.-Alta de Pacientes"
     med="2.-Alta de Medicos"
     hosp="3.-Alta de datos de Hospital"
     sal="4.-Salir"
     #Los cY y cX son para encontrar el centro de la pantalla dependiendo y colocar cada linea donde es
-    cY=yM+2;
-    cY=cY//2;
-    cX=int(len(hosp));
-    cX=xM-cX;
-    cX=cX//2;
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY)+pac+"    ", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+1)+med+"    ", end="");
-    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX+2,cY+2)+hosp+" <--", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+3)+sal+"    ", end="");
+    cY=(y+yM)//2;
+    cX=(x+xM)//2;
+    aux = len(hosp)//2;
+    cX = cX - aux;
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY)+pac+"    ", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+1)+med+"    ", end="");
+    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX,cY+2)+hosp+" <--", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+3)+sal+"    ", end="");
+
 #Crea las diferencias de menu, dependiendo de donde se encuentre la opcion 
 def opmenu4(xM,yM):
+    x=(120-xM)//2+4;
+    y=(30-yM)+2;
     pac="1.-Alta de Pacientes"
     med="2.-Alta de Medicos"
     hosp="3.-Alta de datos de Hospital"
     sal="4.-Salir"
     #Los cY y cX son para encontrar el centro de la pantalla dependiendo y colocar cada linea donde es
-    cY=yM+2;
-    cY=cY//2;
-    cX=int(len(hosp));
-    cX=xM-cX;
-    cX=cX//2;
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY)+pac+"    ", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+1)+med+"    ", end="");
-    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX+2,cY+2)+hosp+"    ", end="");
-    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX+2,cY+3)+sal+" <--", end="");
+    cY=(y+yM)//2;
+    cX=(x+xM)//2;
+    aux = len(hosp)//2;
+    cX = cX - aux;
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY)+pac+"    ", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+1)+med+"    ", end="");
+    print(Fore.WHITE+Back.BLACK+Style.BRIGHT+Cursor.POS(cX,cY+2)+hosp+"    ", end="");
+    print(Fore.BLACK+Back.WHITE+Cursor.POS(cX,cY+3)+sal+" <--", end="");
     
 
 def iniciar(): #Pide los datos para crear la ventana
@@ -154,8 +177,8 @@ def iniciar(): #Pide los datos para crear la ventana
         xM=str(input("Dame la medida horizontal: "));
         try:
             xM=int(xM);
-            if xM > 105 or xM < 60:
-                print("El tamaño debe ser entre 60 y 105");
+            if xM < 0 or xM > 108:
+                print("El tamaño debe ser entre 0 y 108");
             else:
                 a=2;
         except:
@@ -165,8 +188,8 @@ def iniciar(): #Pide los datos para crear la ventana
         yM=str(input("Dame la medida Vertical: "));
         try:
             yM=int(yM);
-            if yM > 23 or yM < 14:
-                print("El tamaño debe ser entre 14 y 23");
+            if yM < 0 or yM > 27:
+                print("El tamaño debe ser entre 0 y 27");
             else:
                 a=2;
         except:
